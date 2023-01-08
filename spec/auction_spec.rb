38f4f1346db4de3cca9b604317b9fc325de94200
Meredith Trevino <meredithtrevino@gmail.com>
2023-01-08 14:15:04 -0700

    adds initialize and items methods to auction class

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,1 +1,15 @@
 require './lib/auction'
+
+RSpec.describe Auction do
+  let(:auction) {Auction.new}
+  
+  describe '#initialize' do
+    it 'exists' do
+      expect(auction).to be_an_instance_of(Auction)
+    end
+    
+    it 'starts with no items' do
+      expect(auction.items).to eq([])
+    end
+  end
+end
