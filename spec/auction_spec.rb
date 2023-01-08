ddad685f66fb8be44248990f851cb5cceccdbd34
Meredith Trevino <meredithtrevino@gmail.com>
2023-01-08 14:20:42 -0700

    Adds completed iteration 1, with working item_name method for auction class

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,8 +1,10 @@
 require './lib/auction'
+require './lib/item'
 
 RSpec.describe Auction do
   let(:auction) {Auction.new}
-  
+  let(:item1) { Item.new('Chalkware Piggy Bank') }
+  let(:item2) { Item.new('Bamboo Picture Frame') }
   describe '#initialize' do
     it 'exists' do
       expect(auction).to be_an_instance_of(Auction)
@@ -10,6 +12,18 @@ RSpec.describe Auction do
     
     it 'starts with no items' do
       expect(auction.items).to eq([])
+    end
+
+    it 'can add items' do
+      auction.add_item(item1)
+      auction.add_item(item2)
+      expect(auction.items).to eq([item1, item2])
+    end
+
+    it 'can return list of item names' do
+      auction.add_item(item1)
+      auction.add_item(item2)
+      expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
     end
   end
 end
